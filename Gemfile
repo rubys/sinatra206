c334a299dac3117cbc1414c36978480b4cebb111
Sam Ruby <rubys@intertwingly.net>
2019-08-21 15:27:54 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+gem 'sinatra', '2.0.6'
+
+group :test do
+  gem 'rake'
+  gem 'rspec'
+end
---
+++ spec/main_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../main'
+
+describe "sinatra" do
+  it "should world" do
+    expect(true).to_be true
+  end
+end
